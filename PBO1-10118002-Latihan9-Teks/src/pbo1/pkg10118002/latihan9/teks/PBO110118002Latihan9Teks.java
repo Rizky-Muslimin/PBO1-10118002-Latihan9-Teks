cee604acdb07d3d996bb934fa6ef68b4c541c82f
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:38:37 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan9-Teks/src/pbo1/pkg10118002/latihan9/teks/PBO110118002Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan9.teks;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    char c;
+    char tab = '\t';
+    char newline = '\n';
+    c = 'A';
+        System.out.println("Hasil" + newline +"Char:" + tab + "berisi " + c);
+    }
+    
+}
